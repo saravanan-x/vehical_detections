173fe980482accf5994d609b44f3108664ccf6b8
saravanan-x <thmurugan481@gmail.com>
2026-04-07 14:14:24 +0530

    test01

--- receiver2.py
+++ receiver2.py
@@ -2,20 +2,23 @@ import cv2
 import requests
 from ultralytics import YOLO
 
-# ESP32 IP
-ESP_IP = "192.168.1.4"
+ESP_IP = "192.168.1.3"
+stream_url = f"http://{ESP_IP}:80"
 
-# Load YOLO model
 model = YOLO("best.pt")
 
-# Camera stream
-stream_url = f"http://{ESP_IP}:80"
 
 cap = cv2.VideoCapture(stream_url)
 
 if not cap.isOpened():
     print("ERROR: Cannot open ESP32 stream")
     exit()
+
+cv2.namedWindow("ESP32 YOLO Detection", cv2.WINDOW_NORMAL)
+
+# Set window size (WIDTH, HEIGHT)
+cv2.resizeWindow("ESP32 YOLO Detection", 1200, 800)
+
 
 while True:
 
@@ -25,7 +28,10 @@ while True:
         print("Frame not received")
         continue
 
-    # Run YOLO detection
+    #  Rotate frame (fix orientation)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
     results = model(frame, conf=0.25)
 
     large_object = False
@@ -35,16 +41,12 @@ while True:
             continue
 
         for box in r.boxes:
-
-            # convert tensor to numbers
             x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
-
             area = (x2 - x1) * (y2 - y1)
 
             if area > 50000:   # adjust threshold if needed
                 large_object = True
 
-    # Control buzzer
     try:
         if large_object:
             requests.get(f"http://{ESP_IP}/buzzer_on", timeout=0.1)
@@ -53,7 +55,7 @@ while True:
     except:
         pass
 
-    # Draw detections
+ 
     annotated = results[0].plot()
 
     cv2.imshow("ESP32 YOLO Detection", annotated)
@@ -62,5 +64,8 @@ while True:
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
+# -----------------------------
+# Cleanup
+# -----------------------------
 cap.release()
 cv2.destroyAllWindows()
